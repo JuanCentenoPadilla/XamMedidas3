e218f519247dff4bdf300ca5bcd936c71ab6a97c
JuanCentenoPadilla <juancentenopadilla@gmail.com>
2019-01-17 12:08:35 +0100

    Update DetallePage.xaml.cs

--- XamMedidas2/XamMedidas2/DetallePage.xaml.cs
+++ XamMedidas2/XamMedidas2/DetallePage.xaml.cs
@@ -151,7 +151,7 @@ namespace XamMedidas2
             int MiContador;
             for (MiContador = 0; MiContador < imagenes.Count; MiContador++)
             {
-                MandarImagenAlServidor(imagenes[MiContador].ImagenPath.ToString(),"fotoscorreo/" + imagenes[MiContador].ImagenNombre + ".jpg");
+                MandarImagenAlServidor(imagenes[MiContador].ImagenPath.ToString(),"fotoscorreo", imagenes[MiContador].ImagenNombre);
             }
             DisplayAlert("Resultado...", string.Format("Enviadas {0} imagenes...", MiContador) , "OK");
 
